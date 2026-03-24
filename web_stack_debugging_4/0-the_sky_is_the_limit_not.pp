b8d32b7710cbb42a4a8320f0610cf18c4a3cfcde
EmnaBarezi <e.barezi@alustudent.com>
2026-03-24 14:21:00 +0200

    Fix nginx ULIMIT with service restart

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Fix Nginx to handle more requests by increasing ULIMIT
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx && nginx -s reload',
-  path    => '/usr/local/bin/:/bin/:/usr/sbin/',
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g" /etc/default/nginx; service nginx restart',
+  path    => '/usr/local/bin/:/bin/:/usr/sbin/:/usr/bin/',
 }
